5f8049320c2f7a7fc188ba1b6bb26f322cb48400
Ccyr01 <82624847+Ccyr01@users.noreply.github.com>
2023-05-07 10:28:12 -0600

    signup.js

--- public/signup.js
+++ public/signup.js
@@ -1,15 +1,10 @@
-console.log("hello login");
-//when someone clicks submit connect to backend "server.js"
-
-
-const loginForm = document.getElementById("login");
-loginForm.addEventListener("submit", (event) => {
+const signupForm = document.getElementById("signup");
+signupForm.addEventListener("submit", (event) => {
     //prevent form from refreshing the page
     event.preventDefault();
     const elements = event.target.elements;
     const email = elements.email.value;
     const password = elements.password.value;
-    // console.log(user);
     //check if email or password don't exists
     // if they don't stop running the code
     if(!email || !password){
@@ -20,38 +15,31 @@ loginForm.addEventListener("submit", (event) => {
         password
     }
     console.log(user);
-    // createUser(user);
-    //verify if user exist then open next page
-    // if not deny access
-    verifyUser(user);
+    createUser(user);
 
 
 })
-
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-
-async function verifyUser(user){
-    const response = await fetch(`http://localhost:3000/login`,{
+async function createUser(user){
+    const response = await fetch(`http://localhost:3000/signup`,{
         method:"POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(user),
     });
+
+    
     
     const jsonData = await response.json();
     if(jsonData._id){
         setCookie("userID",jsonData._id,1);
         window.location.href = "http://localhost:3000/"
     }
-    else{
-        alert("Error: "+jsonData.error);
-    }
     console.log(jsonData);
 }
-
